93d619d76651bdf3e8dfd640c87d05ef1f80de7c
tejaswigavhale <tejaswigavhale42@gmail.com>
2025-03-27 17:39:29 +0530

    hero page update

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
 		<div className="h-[75vh] flex flex-col md:flex-row items-center justify-center">
 			<div className="w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
 				<h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
-					Discover Your Next Great Read
+					Student-Friendly Second-Hand Books
 				</h1>
 				<p className="mt-4 text-xl text-zinc-300 text-center lg:text-left">
-					Uncover captivating stories, enriching knowledge, and endless
-					inspiration in our curated collection of books
+					Get affordable second-hand books, study materials, and notes curated
+					especially for students. Save money while learning smarter.
 				</p>
 				<div className="mt-8">
 					<Link
