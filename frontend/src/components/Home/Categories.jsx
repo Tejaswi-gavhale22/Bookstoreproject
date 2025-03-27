af923c7766138781600fb7ce44c96b2292b34fb4
tejaswigavhale <tejaswigavhale42@gmail.com>
2025-03-27 18:27:07 +0530

    add change

--- frontend/src/components/Home/Categories.jsx
+++ frontend/src/components/Home/Categories.jsx
@@ -10,11 +10,14 @@ const Categories = () => {
 
 	return (
 		<div className="mt-8 px-4">
-			<h2 className="text-3xl font-bold text-yellow-100">Explore Categories</h2>
+			<h2 className="text-3xl font-bold text-yellow-100 mb-6">
+				Explore Categories
+			</h2>
 			<div className="grid grid-cols-3 md:grid-cols-6 gap-6 place-items-center">
 				{categories.map((category, index) => (
 					<div key={index} className="flex flex-col items-center">
-						<span className="text-4xl">{category.icon}</span>
+						<span className="text-4xl mb-3">{category.icon}</span>{" "}
+						{/* Space added here */}
 						<p className="mt-2 font-medium">{category.title}</p>
 					</div>
 				))}
