efd837142a0ef516c5d8d5858b14006bed365ddc
tejaswigavhale <tejaswigavhale42@gmail.com>
2025-03-27 18:18:08 +0530

    add

--- frontend/src/components/Home/Categories.jsx
+++ frontend/src/components/Home/Categories.jsx
@@ -9,10 +9,8 @@ const Categories = () => {
 	];
 
 	return (
-		<div className="mt-8">
-			<h2 className="text-2xl font-bold text-center mb-6">
-				Explore Categories
-			</h2>
+		<div className="mt-8 px-4">
+			<h2 className="text-3xl font-bold text-yellow-100">Explore Categories</h2>
 			<div className="grid grid-cols-3 md:grid-cols-6 gap-6 place-items-center">
 				{categories.map((category, index) => (
 					<div key={index} className="flex flex-col items-center">
